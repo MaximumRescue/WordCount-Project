469a32579098db2650784ecde7ef9775adde8eb9
MaximumRescue <1326314591@qq.com>
2018-03-19 18:49:09 +0800

    Extra options added
    Window command, stoplist are added

---
+++ ExtraOpt.py
@@ -0,0 +1,32 @@
+import sys, wx
+from os.path import join
+
+# get the preserved words list
+def get_prelist():
+    filename = join(sys.path[0], "StopList.txt").replace('\\', '\\\\')
+    fhandle = open(filename, 'r')
+    wholestr = fhandle.read()
+    prelist = wholestr.split(' ')
+    for word in prelist:
+        if word == "" or word == "\n" or word == "\t":
+            prelist.remove(word)
+    return prelist
+
+# A method to get filename by dialog
+def dialog_get():
+    app = wx.App()
+    frame = wx.Frame(None)
+    openwildcard = "All files(*.*)|*.*|" + \
+                    "C/C++ files(*.c;*.cpp;*.h)|*.c;*cpp;*.h|" + \
+                    "Java source files(*.java)|*.java|" + \
+                    "Python source files(*.py)|*.py|" + \
+                    "Text files(*.txt)|*.txt"
+    # Create open file dialog
+    openFileDialog = wx.FileDialog(frame, "Choose a file to open",
+                                   wildcard=openwildcard, 
+                                   style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+    openFileDialog.ShowModal()
+    dir = openFileDialog.GetDirectory()
+    filename = openFileDialog.GetFilename()
+    openFileDialog.Destroy()
+    return dir, filename
