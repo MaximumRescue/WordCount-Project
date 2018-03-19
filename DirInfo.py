e4374e55d72045cec13679a94079ef9f49cd6e71
MaximumRescue <1326314591@qq.com>
2018-03-19 22:31:18 +0800

    Update DirInfo.py
    add the method to deal with inner packages by recursion, and choose the file by its type.

--- DirInfo.py
+++ DirInfo.py
@@ -7,22 +7,43 @@ class DirInfo:
         self.path = pathname
     
     # build up the file list in this directory
-    def build_filelist(self):
+    def build_filelist(self, type=''):
         # get all files and directories from the path
         all_list = listdir(self.path)
         # get a file instead of a directory
         files = []
         for file in all_list:
-            if isfile(join(self.path, file)):
+            if type != '':
+                if isfile(join(self.path, file)) and file.endswith(type):
+                    files.append(file)
+            elif isfile(join(self.path, file)):
                 files.append(file)
         return files
     
+    # build up the directory list in this directory
+    def build_dirlist(self):
+        # get all files and directories from the path
+        all_list = listdir(self.path)
+        # get a file instead of a directory
+        dirs = []
+        for file in all_list:
+            if isfile(join(self.path, file)) == False:
+                dirs.append(file)
+        return dirs  
+      
     # build up the information list from the file list
-    def build_infolist(self):
-        filenames = self.build_filelist()
+    def build_infolist(self, type=''):
+        filenames = self.build_filelist(type)
+        dirnames = self.build_dirlist()
         # get information by creating a FileInfo object
         infos = []
         for fname in filenames:
-            info = FileInfo(fname)
+            info = FileInfo(self.path, fname)
             infos.append(info)
+        # deal with the inner directory
+        for dirname in dirnames:
+            new_path = join(self.path, dirname)
+            newdir = DirInfo(new_path)
+            new_infos = newdir.build_infolist(type)
+            infos = infos + new_infos
         return infos
